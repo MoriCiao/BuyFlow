35de6698fbfdad89dc03fb01f6ee3d2f24901bb8
MoriCiao <bboynoice@gmail.com>
2025-09-20 19:40:42 +0800

    - refactor: 版面重構     - OrderItems.jsx 內容重構

--- src/components/OrderTracking/OrderItems.jsx
+++ src/components/OrderTracking/OrderItems.jsx
@@ -3,35 +3,24 @@ import React from "react";
 const OrderItems = ({ items, totalQuatity }) => {
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex items-center justify-between">
+      <div className="flex flex-none items-center justify-between">
         <p>📦 訂單內容</p>
-        <span className="rounded-md bg-zinc-600 p-1">{totalQuatity}件</span>
+        <span className="rounded-md bg-green-600 p-1 px-2">
+          {totalQuatity}件
+        </span>
       </div>
-      <div className="m-auto flex max-h-[15vh] w-full gap-4 overflow-y-auto">
+      <div className="flex w-full flex-col gap-2 overflow-y-auto rounded-md bg-zinc-600 p-2 lg:max-h-30">
         {items &&
           items.map((i, index) => {
             return (
               <div
                 key={index}
-                className="flex w-full items-start justify-between rounded-xl border border-white/20 p-2 transition duration-300 hover:border-white hover:bg-zinc-500"
+                className="flex w-full items-start justify-between border-b border-white/50 px-4 shadow-xl transition duration-300 hover:border-white hover:bg-zinc-500"
               >
-                <div className="flex items-center">
-                  <div className="rounded-xl bg-zinc-600 p-2">
-                    <img src={i.image} alt="items-image" className="w-5" />
-                  </div>
-                  <div>
-                    <div className="col-span-2 flex flex-col justify-between px-4">
-                      <p>
-                        {i.name} <br /> {i.quantity} 個
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex h-full items-center">
-                  <p className="text-start">
-                    <strong>{i.quantity * i.price} $</strong>
-                  </p>
-                </div>
+                <img src={i.image} alt="items-image" className="w-5" />
+                <p>{i.name}</p>
+                <p>* {i.quantity} 個</p>
+                <p>{i.quantity * i.price} $</p>
               </div>
             );
           })}
