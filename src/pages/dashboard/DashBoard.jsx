33cd77770788c766086cf8bc94e89d4be6d2929a
MoriCiao <bboynoice@gmail.com>
2025-09-21 15:18:10 +0800

    - rafactor: 重構及整理程式碼     - DashBoard.jsx - 元件拆分 & Class整合為 STYLE

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -6,75 +6,87 @@ const DashBoard = () => {
   const { user } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const location = useLocation();
+
+  const STYLE = {
+    dashboard: `dashboard relative flex h-full min-h-[90vh] w-full flex-col gap-4 rounded-md bg-zinc-800 p-8 lg:flex-row`,
+
+    dashboard_ul: `flex w-full flex-wrap gap-4 lg:flex-col lg:justify-start`,
+
+    dashboard_container: `dashboard-container flex h-auto w-full flex-4 overflow-auto text-white`,
+
+    dashboard_bg_container: `dashboard-bg-container flex h-full w-full items-center`,
+
+    dashboard_bg: `dashboard-bg objcet-cover w-full opacity-30 select-none`,
+  };
+
+  const DashboardNav = () => (
+    <nav className="dashboard-nav flex flex-col lg:flex-1">
+      <ul className={STYLE.dashboard_ul}>
+        <li>
+          <Button
+            label="Staff List"
+            size="lg"
+            onClick={() => navigate("stafflist")}
+          />
+        </li>
+        <li>
+          {user?.role === "admin" ? (
+            <Button
+              label="Admin Profile"
+              size="lg"
+              onClick={() => navigate("admin")}
+            />
+          ) : (
+            <Button
+              label="Staff Profile"
+              size="lg"
+              onClick={() => navigate("staff")}
+            />
+          )}
+        </li>
+        <li>
+          <Button
+            label="Products List"
+            size="lg"
+            onClick={() => navigate("productslist")}
+          />
+        </li>
+        <li>
+          <Button
+            label="Menber List"
+            size="lg"
+            onClick={() => navigate("menberlist")}
+          />
+        </li>
+        <li>
+          <Button
+            label="Order List"
+            size="lg"
+            onClick={() => navigate("orderlist")}
+          />
+        </li>
+      </ul>
+    </nav>
+  );
+
+  const DashBoardBg = () => (
+    <div className={STYLE.dashboard_bg_container}>
+      <img
+        draggable="false"
+        src="/BuyFlow/dashboard-bg.svg"
+        alt="dashboard-bg"
+        className={STYLE.dashboard_bg}
+      />
+    </div>
+  );
   return (
-    <section className="dashboard relative flex h-full min-h-[90vh] w-full flex-col gap-4 rounded-md bg-zinc-800 p-8 lg:flex-row">
+    <section className={STYLE.dashboard}>
       {/* Dashboard Nav */}
-      <nav className="dashboard-nav flex flex-col lg:flex-1">
-        <ul className="flex w-full flex-wrap gap-4 lg:flex-col lg:justify-start">
-          <li>
-            <Button
-              label="Staff List"
-              size="lg"
-              onClick={() => navigate("stafflist")}
-            />
-          </li>
-          <li>
-            {user?.role === "admin" ? (
-              <Button
-                label="Admin Profile"
-                size="lg"
-                onClick={() => navigate("admin")}
-              />
-            ) : (
-              <Button
-                label="Staff Profile"
-                size="lg"
-                onClick={() => navigate("staff")}
-              />
-            )}
-          </li>
-          <li>
-            <Button
-              label="Products List"
-              size="lg"
-              onClick={() => navigate("productslist")}
-            />
-          </li>
-          <li>
-            <Button
-              label="Menber List"
-              size="lg"
-              onClick={() => navigate("menberlist")}
-            />
-          </li>
-          <li>
-            <Button
-              label="Order List"
-              size="lg"
-              onClick={() => navigate("orderlist")}
-            />
-          </li>
-        </ul>
-      </nav>
+      <DashboardNav />
 
       {/* Dashboard Container */}
-      <div className="dashboard-container flex h-full w-full flex-4 overflow-auto text-white">
-        {location.pathname === "/dashboard" ? (
-          <div className="dashboard-bg relative h-full w-full">
-            <img
-              draggable="false"
-              src="/BuyFlow/dashboard-text.svg"
-              alt="dashboard-text"
-              className="objcet-cover absolute left-1/2 z-10 w-180 -translate-x-1/2 rotate-[-20deg] opacity-80 select-none sm:top-25 md:top-45 xl:top-50"
-            />
-            <img
-              draggable="false"
-              src="/BuyFlow/dashboard-bg.svg"
-              alt="dashboard-bg"
-              className="objcet-cover m-auto opacity-30 select-none"
-            />
-          </div>
-        ) : null}
+      <div className={STYLE.dashboard_container}>
+        {location.pathname === "/dashboard" ? <DashBoardBg /> : null}
 
         <Outlet />
       </div>
