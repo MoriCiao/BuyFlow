541bd1934ed86cc494c845810342e1c63aa08c31
MoriCiao <bboynoice@gmail.com>
2025-09-23 10:03:48 +0800

    refactor: 調整 FeaturedModal 元件樣式及結構

--- src/components/Featured/FeaturedModal.jsx
+++ src/components/Featured/FeaturedModal.jsx
@@ -4,9 +4,13 @@ import { addItem } from "../../features/cart/cartSlice";
 import Button from "../Button/Button";
 
 const STYLE = {
-  featured_mask: `featured-modal-mask fixed inset-0 top-0 left-0 z-50 flex items-center justify-center overflow-hidden bg-black/50 2xl:p-20`,
+  featured_mask: `featured-modal-mask fixed inset-0 top-0 left-0 z-60 flex items-center justify-center overflow-hidden bg-black/50 2xl:p-20`,
 
-  featured_modal: `featured-modal flex w-[70%]  flex-col items-center justify-center rounded-xl border border-white/50 bg-zinc-800 xl:w-[60%]`,
+  featured_modal: `featured-modal flex w-[80%] flex-col items-center justify-center rounded-xl border border-white/50 bg-zinc-800 md:w-[40%]`,
+
+  featured_modal_container: `flex w-full gap-2 px-4 `,
+
+  featured_modal_content: `flex flex-1 flex-col justify-between gap-2 py-4 text-end`,
 };
 
 const FeaturedModal = ({ isFeaturedModal, setIsFeaturedModal }) => {
@@ -25,16 +29,16 @@ const FeaturedModal = ({ isFeaturedModal, setIsFeaturedModal }) => {
               ❌
             </span>
           </div>
-          <div className="flex w-full flex-col gap-4 px-4 md:flex-row">
-            <div className="flex-1">
+          <div className={STYLE.featured_modal_container}>
+            <div className="flex flex-1 items-center py-4">
               <img
                 src={product.image}
                 alt="精選商品"
-                className="m-auto w-80 rounded-xl border border-white/50 bg-zinc-700 p-8"
+                className="m-auto w-60 p-8 md:w-80"
               />
             </div>
-            <div className="flex flex-1 flex-col justify-between gap-4 border-t py-4 text-end md:border-0">
-              <h3 className="text-[1.5rem] md:text-[2rem]">
+            <div className={STYLE.featured_modal_content}>
+              <h3 className="text-[1.25rem] md:text-[1.5rem]">
                 <strong className="text-red-600">Hot {product.rating} !</strong>{" "}
                 <br className="sm:hidden" />
                 {product.name}
@@ -46,12 +50,11 @@ const FeaturedModal = ({ isFeaturedModal, setIsFeaturedModal }) => {
               </p>
 
               <div className="flex items-center justify-end gap-4">
-                <strong className="text-[1.5rem] text-red-600 sm:text-[2rem]">
-                  {product.price} $
-                </strong>
+                <strong className="text-red-600">{product.price} $</strong>
                 <div>
                   <Button
                     label="ADD"
+                    size="sm"
                     onClick={() => {
                       dispatch(addItem({ product, quantity: 1 }));
                       setIsFeaturedModal({ open: false, product: null });
