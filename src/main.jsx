64897b8dce4ded88a22fff7a2c2365bbb77062d0
MoriCiao <bboynoice@gmail.com>
2025-09-25 10:10:24 +0800

    fix: 路由BrowserRouter 換成 HashRouter，避免頁面更新時顯示404

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  // </StrictMode>
-  <BrowserRouter basename="/BuyFlow/">
+  <HashRouter basename="/">
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
