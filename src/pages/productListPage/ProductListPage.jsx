90acf9bafa52cff662ae8daaadb22ec45bfc8c3a
MoriCiao <bboynoice@gmail.com>
2025-10-01 13:57:28 +0800

    refactor: 將useState轉移至父元件

--- src/pages/productListPage/ProductListPage.jsx
+++ src/pages/productListPage/ProductListPage.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
 import "./_ProductListPage.scss";
 import CategoryBtn from "../../components/CategoryBtn/CategoryBtn.jsx";
 import CategoryFilter from "../../components/CategoryFilter/CategoryFilter.jsx";
 
 // 所有商品的 List, 會使用 <ProductCard/>
 const ProductListPage = () => {
+  const [activeBtn, setActiveBtn] = useState("");
   return (
     <section className="product-list gap-2 text-white">
       <nav className="category_filter_nav">
-        <CategoryBtn />
+        <CategoryBtn activeBtn={activeBtn} setActiveBtn={setActiveBtn} />
       </nav>
-
       <div className="category_product">
         <CategoryFilter />
       </div>
